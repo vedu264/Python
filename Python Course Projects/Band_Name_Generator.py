783666660b3e3e4a7b3913b7c18b6d29b6916f73
vedu264 <gvedant@bu.edu>
2024-02-22 01:07:36 -0500

    Project

---
+++ Python Course Projects/Band_Name_Generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+cityname = input("What's name of the city you grew up in?")
+print(cityname)
+petname = input("What's your pet's name?")
+print(petname)
+print("Your band name could be " + cityname + " " + petname)
